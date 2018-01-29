057b0d4375622bf99b4be68216b4df82789db298
Brandon A. Durepo <bdurepo@gatech.edu>
2018-01-28 18:59:34 -0800

    Added matrix of os and version

--- packager/testing/test_scripts/build_repo_packages.sh
+++ packager/testing/test_scripts/build_repo_packages.sh
@@ -6,19 +6,23 @@ export BUILD_DIR=`pwd`/packager/testing/test_scripts
 
 # Create a build container
 # Build will execute in the container by mapping repository volumes
-docker run --privileged  -d -ti -e "container=docker" \
-	-v /tmp/depot_tools:/depot_tools:rw \
-	-v $BUILD_DIR/artifacts:/artifacts:rw \
-       	-v $BUILD_DIR/resources:/resources:rw \
-	centos:centos7 /usr/sbin/init
+
+if [ ${OS_TYPE} == "centos" ] then;
+	docker run --privileged  -d -ti -e "container=docker" \
+		-v /tmp/depot_tools:/depot_tools:rw \
+		-v $BUILD_DIR/artifacts:/artifacts:rw \
+       		-v $BUILD_DIR/resources:/resources:rw \
+		centos:centos${OS_VERSION} /usr/sbin/init
+fi
 
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
-
 docker logs $DOCKER_CONTAINER_ID
 
 # Execute the build and test script in the build container
-docker exec -ti $DOCKER_CONTAINER_ID \
-	/bin/bash -xec "bash -xe /resources/build_centos_rpm.sh"
+if [ ${OS_TYPE} == "centos" ] then;
+	docker exec -ti $DOCKER_CONTAINER_ID \
+		/bin/bash -xec "bash -xe /resources/build_centos_rpm.sh"
+fi
 
 # Do cleanup
 docker ps -a
